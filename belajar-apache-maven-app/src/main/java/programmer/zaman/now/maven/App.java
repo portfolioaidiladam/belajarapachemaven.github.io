c5e22806240cbd29341ff3553c096d983b94efc0
portfolioaidiladam <portfolioaidiladam@gmail.com>
2024-08-02 14:43:48 +0700

    first commit

---
+++ belajar-apache-maven-app/src/main/java/programmer/zaman/now/maven/App.java
@@ -0,0 +1,19 @@
+package programmer.zaman.now.maven;
+
+import com.google.gson.Gson;
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Gson gson =new Gson();
+
+        Person person = new Person ("Aidil Adam");
+        String json = gson.toJson(person);
+
+        System.out.println(json);
+    }
+}
